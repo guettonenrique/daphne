a4c8453bea0055c8e194d201c58b7c2d1f1599c2
guettonenrique <guettonenrique@gmail.com>
2026-09-21 20:11:13 -0600

    Initial Commit

---
+++ daphne/daphneApp.swift
@@ -0,0 +1,17 @@
+//
+//  daphneApp.swift
+//  daphne
+//
+//  Created by Enrique on 9/21/26.
+//
+
+import SwiftUI
+
+@main
+struct daphneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
